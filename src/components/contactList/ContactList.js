ab9d11f6290a9d037380ebc7e97756d974944190
Nikolaylyah <107050313+Nikolaylyah@users.noreply.github.com>
2023-01-19 17:14:32 +0200

    Update ContactList.js

--- src/components/contactList/ContactList.js
+++ src/components/contactList/ContactList.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { List, ListItem, Text, ItemBtn } from './ContactList.styled';
 
+
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <List>
